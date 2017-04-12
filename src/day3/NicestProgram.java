bfb7aa1bdfce059c52db0b03115d8bae04a3e2d1
ColeTrammer <cole.trammer@gmail.com>
2017-04-12 14:59:36 -0700

    3

---
+++ src/day3/NicestProgram.java
@@ -0,0 +1,11 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "You figured it out!");
+		JOptionPane.showMessageDialog(null, "You are so smart!");
+		JOptionPane.showMessageDialog(null, "Yay!");
+	}
+}
